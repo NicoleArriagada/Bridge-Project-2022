eb91c3bb99058ede11aecf7850df03cf996c85f8
NicoleArriagada <nicole.magdalena@gmail.com>
2022-04-03 16:21:53 -0400

    tarjetas automatizadas con js

--- dist/javascript/script.js
+++ dist/javascript/script.js
@@ -7,7 +7,46 @@ $(function(){
   $("#footer-placeholder").load("footer.html")
 })
 
-console.log("test");
+var dataBase = [
+  {'img':'./dist/image/design_banners/color-hex_banner.JPG','link':'https://www.color-hex.com','title':'Color-Hex','texto':'You can study various programming languages here, ranging from HTML to SQL. This site has plenty of practical examples for you to practice.'}
+, {'img':'./dist/image/design_banners/coolors_banner.JPG','link':'https://www.coolors.co','title':'Coolors','texto':'You can study various programming languages here, ranging from HTML to SQL. This site has plenty of practical examples for you to practice.'}
+, {'img':'./dist/image/design_banners/colordrop_banner.JPG','link':'https://colordrop.io','title':'Color Drop','texto':'You can study various programming languages here, ranging from HTML to SQL. This site has plenty of practical examples for you to practice.'}
+, {'img':'./dist/image/design_banners/canva_banner.JPG','link':'https://www.canva.com','title':'Canva','texto':'You can study various programming languages here, ranging from HTML to SQL. This site has plenty of practical examples for you to practice.'}
+, {'img':'./dist/image/design_banners/coolors_banner.JPG','link':'https://www.coolors.co','title':'Coolors','texto':'You can study various programming languages here, ranging from HTML to SQL. This site has plenty of practical examples for you to practice.'}
+, {'img':'./dist/image/design_banners/colordrop_banner.JPG','link':'https://colordrop.io','title':'Color Drop','texto':'You can study various programming languages here, ranging from HTML to SQL. This site has plenty of practical examples for you to practice.'}
+, {'img':'imagen1', 'link':'pongo link','title':'titulo', 'text':'blabla'}
+]
+class MainFill {
+  constructor () {
+      const main = document.getElementById('mainrow');
+      let divContent = '';
+      dataBase.forEach(element => { 
+        divContent += `<div class="col-sm-12 cold-md-6 col-xl-4">         
+      <div class="myRscCard shadow p-3 mb-5 bg-body rounded">
+        <div class="image">
+         <img src="${element.img}" class="image__img card-img-top" alt="color-hex_banner">
+         <a href="${element.link}" target="_blank">                   
+          <div class="image__overlay image__overlay--blur">
+            <div class="image__title">${element.title}</div> 
+          </div>
+        </a> 
+        </div>                             
+        <div class="card-body">
+          <p class="card-text">${element.texto}</p>
+        </div>
+      </div>
+    </div>`;
+  
+      });    
+  for (let index = 0; index < 3; index++) {
+            
+  }
+      
+  main.innerHTML = divContent;
+  }  
+}
+var mainObj = new MainFill();
+
 
 /*let namePerson; 
 console.log(namePerson);
